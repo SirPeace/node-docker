7a3ee9ec80db3a0dd5c415b09785a58bc8ce91fb
SirPeace <roman.khabibulin12@gmail.com>
2021-09-21 13:45:40 +0300

    Add mongoose and check connection to DB
    - You may reference container IP address by its service name within defined network

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const app = require("express")()
+const mongoose = require("mongoose")
+
+const dbService = "database"
+const dbServicePort = 27017
+const dbUsername = "sirpeace"
+const dbPassword = "password"
+
+mongoose
+    .connect(
+        `mongodb://${dbUsername}:${dbPassword}` +
+        `@${dbService}:${dbServicePort}` +
+        "/?authSource=admin"
+    )
+    .then(() => console.log('Successfully connected to the database!'))
+    .catch(e => console.error(e))
 
 app.get('/', (req, res) => {
     res.send("<h2>Hi There!!!</h2>")
